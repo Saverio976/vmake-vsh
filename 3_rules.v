59b9ce926cde8ab520b0ebc00fd2dcc7be5e462c
Xavier Mitault <xavier.mitault@epitech.eu>
2025-06-15 17:26:45 +0100

    Add timeit

--- 3_rules.v
+++ 3_rules.v
@@ -13,10 +13,10 @@ struct Rules {
 fn (r Rules) all(args Args) ! {
 }
 
-@[deps: 'vmake.v	v.mod']
+@[deps: 'v.mod	1_args.v	2_main.v	3_rules.v']
 @[name: 'vmake']
 fn (r Rules) target(args Args) ! {
-	args.sh(@VEXE + ' -prod .')!
+	args.sh(@VEXE + ' -prod .', timeit: true)!
 }
 
 @[phony]
